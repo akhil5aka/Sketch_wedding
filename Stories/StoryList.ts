218070fab90b0ad8adf6a68b8a3e6c0a3eba16d5
akhil5aka <akhil5aka@users.noreply.github.com>
2025-07-31 07:30:57 +0000

    deploy: 226e6cb02d42bcf30487e9fce23d2d4ffb57b510

---
+++ Stories/StoryList.ts
@@ -0,0 +1,23 @@
+
+
+export const stories = [
+  {
+    slug: "celine-steven",
+    image: "/Stories/celine-steven/thumbnail.jpeg",
+    couple: "CELINE STEVEN",
+    date: "12TH JULY, 2025",
+  },
+  {
+    slug: "sanya-ashish",
+    image: "/Stories/sanya-ashish/thumbnail.jpeg",
+    couple: "SANYA ASHISH",
+    date: "15TH JUNE, 2025",
+  },
+  {
+    slug: "nidhi-vaibhav",
+    image: "/Stories/nidhi-vaibhav/thumbnail.jpeg",
+    couple: "NIDHI VAIBHAV",
+    date: "5TH JUNE, 2025",
+  },
+  
+];
